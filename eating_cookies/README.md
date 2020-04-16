c2e136732eb09951e9ccac99dd937d80a6c88bfc
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-04-15 18:48:50 -0700

    Eating Cookies

--- eating_cookies/README.md
+++ eating_cookies/README.md
@@ -1,4 +1,6 @@
-# Eating Cookies
+ # Eating Cookies
+
+ Looking for combinations, probably exponential 3^n
 
 Cookie Monster can eat either 0, 1, 2, or 3 cookies at a time. If he were given a jar of cookies with `n` cookies inside of it, how many ways could he eat all `n` cookies in the cookie jar? Implement a function `eating_cookies` that counts the number of possible ways Cookie Monster can eat all of the cookies in the jar. 
 
@@ -8,6 +10,30 @@ For example, for a jar of cookies with `n = 3` (the jar has 3 cookies inside it)
  2. He can eat 1 cookie, then 2 cookies 
  3. He can eat 2 cookies, then 1 cookie
  4. He can eat 3 cookies all at once. 
+
+ n = 1
+ 1. 1 cookie 1 time
+
+ n = 2
+ 1. 1 cookie 1 time 1 cookie 1 time
+ 2. 2 cookie 1 time
+
+ n = 3
+ 1. 1 cookie 1 time 1 cookie 1 time 1 cookie 1 time
+ 2. 2 cookie 1 time 1 cookie 1 time
+ 3. 1 cookie 1 time 2 cookie 1 time
+ 4. 3 cookie 1 time
+
+ n = 4 
+ 1. 1 cookie 1 time 1 cookie 1 time 1 cookie 1 time 1 cookie 1 time
+ 2. 2 cookie 1 time 1 cookie 1 time 1 cookie 1 time
+ 3. 1 cookie 1 time 2 cookie 1 time 1 cookie 1 time
+ 4. 3 cookie 1 time 1 cookie 1 time
+ 5. 1 cookie 1 time 1 cookie 1 time 2 cookie 1 time
+ 6. 2 cookie 1 time 2 cookie 1 time
+ 7. 1 cookie 1 time 3 cookie 1 time
+
+e_c(n) = e_c(n-1) + e_c(n-2) + e_c(n-3)
 
 Thus, `eating_cookies(3)` should return an answer of 4.
 
--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,12 +5,35 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+# Each problem is the samation of the previous problem
 def eating_cookies(n, cache=None):
-  pass
+  #base cases
+  if cache is None:
+    cache = {0: 1,  1: 1,  2: 2,  3: 4}
+  if n == 0:
+      return 1
+  elif n == 1:
+      return 1
+  elif n == 2:
+      return 2
+  elif n == 3:
+      return 4
+  # elif cache: 
+  elif cache and cache[n]:
+      return cache[n]
+  else: 
+      cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+  
+  
+  # return eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+  
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+  return cache[n]
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
+print(eating_cookies(3, {}))
